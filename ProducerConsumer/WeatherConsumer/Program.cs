bc26744562b2543b90336ecf977320735bf15ee3
ayush-lal <ayush.lal@outlook.com>
2023-01-04 13:21:39 +1000

    added consumer code

--- ProducerConsumer/WeatherConsumer/Program.cs
+++ ProducerConsumer/WeatherConsumer/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
 
-Console.WriteLine("Hello, World!");
+var config = new ConsumerConfig
+{
+    GroupId = "weather-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("weather-topic");
+
+var token = new CancellationTokenSource();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+        if (response.Message != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+            Console.WriteLine($"State: {weather.State}, Temparature: {weather.Temparature}");
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+public record Weather(string State, int Temparature);
