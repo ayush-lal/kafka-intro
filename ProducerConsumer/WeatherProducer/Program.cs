237c79eca58468f10e204485f0b1252e3b3ab016
ayush-lal <ayush.lal@outlook.com>
2023-01-04 13:22:09 +1000

    added producer code

--- ProducerConsumer/WeatherProducer/Program.cs
+++ ProducerConsumer/WeatherProducer/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
 
-Console.WriteLine("Hello, World!");
+var config = new ProducerConfig {BootstrapServers = "localhost:9092"};
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    string? state;
+    while ((state = Console.ReadLine()) != null)
+    {
+        var response = producer.ProduceAsync(
+            "weather-topic",
+            new Message<Null, string>()
+            {
+                Value = JsonConvert.SerializeObject(new Weather(state, 56))
+            }
+        );
+    }
+}
+catch (ProduceException<Null, string> ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public record Weather(string State, int Temparature);
